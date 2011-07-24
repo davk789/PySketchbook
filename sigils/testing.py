09b8ca5fd9b40e4ead1447663fbdb7c32222a5ae
davk789 <david.joseph.kendall@gmail.com>
2011-07-24 00:09:41 -0700

    improved capture error handling and namespace conflicts. har

--- sigils/testing.py
+++ sigils/testing.py
@@ -5,22 +5,31 @@ Created on Jul 23, 2011
 '''
 
 import cv
-import time, sys
+#import time
+import sys
 
-cv.NamedWindow("test", cv.CV_WINDOW_AUTOSIZE)
-
+capture = None
 for i in range(5):
+    global capture 
     capture = cv.CaptureFromCAM(i)
     if capture:
         print "i got index ", i  
         break
 
+if not capture:
+    print "No camera captured. Exiting..."
+    sys.exit(1)
+    
+cv.NamedWindow("test", cv.CV_WINDOW_AUTOSIZE)
+    
+
 def get_cam():
     frame = cv.QueryFrame(capture)
+    
     cv.ShowImage("test", frame)
     #time.sleep(1)
     c = cv.WaitKey(10)
-    if c == "q":
+    if c == 'q':
         sys.exit(0)
     
 
