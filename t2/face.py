8f2f7e9df4a4c106b72a353f79e1531c45795012
davk789 <david.joseph.kendall@gmail.com>
2011-08-11 20:04:53 -0700

    at a stopping point with the face code, moving on to synth/osc stuff

--- t2/face.py
+++ t2/face.py
@@ -7,26 +7,133 @@ algorithm from the detected features.
 """
 
 # this would be one way to forbid running scripts as main
-import sys
+import sys, platform, math, random
 
 import cv
 import pygame
-from pygame.locals import *
-
-
 
 class Faces(object):
-    def __init__(self, capture):
+    """
+    Analyze an image using face recognition and points of interest, coming from
+    an opencv image, and then draw a figure on a pygame Surface based on the
+    manipulated data.
+    """
+    def __init__(self):
         super(Faces, self).__init__()
-        self.capture = capture
-        print self.__class__.__string__, "initialized"
+        if platform.system() == 'Windows':
+            self.cascade = cv.Load("G:\\Developer\\OpenCV2.3\\opencv\\data\\haarcascades\\haarcascade_frontalface_default.xml")
+        else:
+            self.cascade = cv.Load("/opt/local/var/macports/build/_opt_local_var_macports_sources_rsync.macports.org_release_ports_graphics_opencv/opencv/work/OpenCV-2.2.0/data/haarcascades/haarcascade_frontalface_default.xml")
         
-    def get_image(self):
-        print "I should return a pygame image to draw the main window."
-        return self
+    def analyze(self, frame):
+        return [self.get_points_of_interest(frame, (x,y,w,h)) 
+                for (x,y,w,h),n 
+                in self.detect_faces(frame)]
+    
+    def get_points_of_interest(self, img, roi, numpoints=15):
+        """
+        Use cv.GoodFeaturesToTrack() to get points over a region of interest,
+        then order and quantize the resulting list.
+        
+        ** All data processing should happen from this function.
+        """
+        cropped = self.crop(img, roi)
+        
+        g_img = self.get_grayscale(cropped)
+        eig_image = cv.CreateMat(g_img.rows, g_img.cols, cv.CV_32FC1)
+        temp_image = cv.CreateMat(g_img.rows, g_img.cols, cv.CV_32FC1)
+        
+        features = cv.GoodFeaturesToTrack(g_img,
+                                          eig_image, 
+                                          temp_image, 
+                                          numpoints,
+                                          0.001, # quality level -- lower is better
+                                          0.01, 
+                                          useHarris = True)
 
+        ret = self.order_points(features, roi)
+        return [self.quantize((x, y), roi[2:]) for x, y in ret]
+        
+    
+    def quantize(self, pt, size, grain=5.0):
+        """
+        There has got to be a more elegant way to quantize a point
+        """
+        stepx = size[0] / grain
+        stepy = size[1] / grain
+        quantx = 0
+        quanty = 0
+    
+        while abs(quantx - pt[0]) > (stepx / 2.0):
+            quantx += stepx
+        while abs(quanty - pt[1]) > (stepy / 2.0):
+            quanty += stepy
+    
+        return (quantx / size[0], quanty / size[1])
+    
+    def order_points(self, pts, roi):
+        indexes = []
+        for pt in pts:
+            indexes.append(self.nearest_index(pt, pts, indexes, roi))
+        return [pts[i] for i in indexes]
+    
+    def nearest_index(self, loc, pts, exclude, roi):
+        min = get_distance((0, 0), roi[2:])
+        lind = 0
+        for ind in range(len(pts)):
+            distance = get_distance(loc, pts[ind])
+            if (distance < min) and (distance > 0) and (not ind in exclude):
+                min = distance
+                lind = ind
+    
+        return lind
+    
+    def crop(self, img, roi):
+        cv.SetImageROI(img, roi)
+        cropped = cv.CreateImage(roi[2:], img.depth, img.nChannels)
+        cv.Copy(img, cropped)
+        cv.ResetImageROI(img)
+        return cropped
+        
+    def detect_faces(self, frame):
+        g_img = self.get_grayscale(frame)
+        storage = cv.CreateMemStorage(0)
+        cv.EqualizeHist(g_img, g_img)
+        return cv.HaarDetectObjects(g_img, self.cascade, storage)
+    
+    def get_grayscale(self, frame):
+        gray = cv.CreateMat(frame.height, frame.width, cv.CV_8U)
+        cv.CvtColor(frame, gray, cv.CV_RGB2GRAY)
+        return gray
+
+    def draw(self, image, data):
+        "scale and draw the data"
+        for face in data:
+            scaled =  [(x*image.get_width(), y*image.get_height()) for x, y in face]
+            pygame.draw.lines(image, 
+                              pygame.Color(random.randrange(0, 255),
+                                           random.randrange(0, 255),
+                                           random.randrange(0, 255)), 
+                              0, # filled
+                              scaled, 
+                              2)
+        
+def get_distance(start, end):
+    a = abs(end[0] - start[0])
+    b = abs(end[1] - start[1])
+    return math.sqrt(pow(a,2) + pow(b, 2))
+
+def test():
+    capture = cv.CaptureFromCAM(0)
+    if not capture:
+        print "Could not get the cam. Crashing now..."
+        sys.exit(1)
+        
+    faec = Faces(capture)
+    print faec.get_image()
+    sys.exit(0)
 
 if __name__ == "__main__":
-    print "run this from trans.py!"
-    sys.exit(1)
+    print "creating the class"
+    test()
 
--- t2/trans.py
+++ t2/trans.py
@@ -1,56 +1,111 @@
 """
 trans.py
 
-Top-level transmutation study (second pass) script.
+Transmutation Study
+
+Analyze faces in a webcam capture. Based on the data, draw a sigil and start a
+synth.
+
+TODO:
+1 - audio programming
+2 - analysis timing and behavior:
+     - draw a sigil when detecting the face and preserve that image - no 
+       re-analysis/redraw
+     - if no face is detected, remove the image and wait for new image
+3 - window resize behavior - test on dual-screen monitor
 
 """
 
 import sys
 
 import pygame
+from pygame.locals import *
 import cv
 
 from face import Faces
 
-if __name__ != "__main__":
-    # i guess this makes sense?
-    print "This script is not to be imported. Run it directly dummy!"
-    sys.exit(2)
 
-CAMWIDTH  = 640
-CAMHEIGHT = 480
-CAMERA    = 0
+WIDTH  = 800
+HEIGHT = 800
+CAMERA = 0
 
-capture = cv.CaptureFromCAM(CAMERA)
-if not capture:
-    print "Could not get the cam. Crashing now..."
-    sys.exit(1)
+def get_capture_image(frame):
+    "convert cv image to pygame image"
+    frame_rgb = cv.CreateMat(frame.height, frame.width, cv.CV_8UC3)
+    cv.CvtColor(frame, frame_rgb, cv.CV_BGR2RGB)
+    return pygame.image.frombuffer(frame_rgb.tostring(), 
+                                   cv.GetSize(frame_rgb), 
+                                   "RGB")
 
-faces = Faces(capture)
+def get_solid_image(dimensions):
+    "generate a blank pygame drawing surface"
+    canvas = pygame.Surface(dimensions)
+    canvas.fill(pygame.Color("black"))
+    return canvas
 
-def get_draw_image():
-    return faces.get_image()
+def toggle_fullscreen(screen):
+    if toggle_fullscreen.full:
+        screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    else:
+        screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    toggle_fullscreen.full = not toggle_fullscreen.full
+    return screen
+toggle_fullscreen.full = False
+
+def update(screen, capture, faces):
+    """prepare images and data, and then update the pygame display"""
+    cv_frame = cv.QueryFrame(capture)
+    data = faces.analyze(cv_frame)
+    
+    has_data = len(data) > 0
+    if has_data == update.last_data:
+        return
+
+    update.last_data = has_data
+    
+    #pg_frame = get_capture_image(cv_frame)
+    pg_frame = get_solid_image((WIDTH, HEIGHT))
+
+    faces.draw(pg_frame, data)
+    screen.blit(pg_frame, (0, 0))
+    pygame.display.flip()
+update.last_data = False
 
 def main():
+    
     pygame.init()
-    screen = pygame.display.set_mode((CAMWIDTH, CAMHEIGHT))
+    
+    capture = cv.CaptureFromCAM(CAMERA)
+    if not capture:
+        print "Could not get the cam. Crashing now..."
+        sys.exit(1)
+    
     pygame.display.set_caption("Transmutation Study")
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-        
+    faces = Faces()
+    img = update(screen, capture, faces)
+
     while True:
         for ev in pygame.event.get():
             if ev.type == QUIT:
                 return
-        
-        img = get_draw_image()
-        screen.blit((0, 0))
-        display.flip()
+            elif ev.type == KEYDOWN:
+                if ev.key == K_f:
+                    screen = toggle_fullscreen(screen)
+                elif ev.key == K_ESCAPE:
+                    return
 
-    #face = FaceTransmuter()
+        update(screen, capture, faces)
+        pygame.time.wait(50)
+
+if __name__ == "__main__":
+    main()
+else:
+    print "This script is not to be imported. Run it directly dummy!"
+    raise Exception("import_error")
 
 
-
-main()
 
 
 
