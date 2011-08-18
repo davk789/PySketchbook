14d4a985e26a67b5a2219a827fe593fec8c10055
davk789 <david.joseph.kendall@gmail.com>
2011-08-18 11:47:14 -0700

    adding audio script to transmutation study

---
+++ t2/synth.py
@@ -0,0 +1,10 @@
+"""
+synth.py
+
+Forget about re-implementing sc's Task. Send data to SuperCollider, do not 
+control supercollider from here. There's no point in reimplementing sc in 
+python. As tempting as it may be, I need to stick with more basic python 
+programming.
+"""
+
+
