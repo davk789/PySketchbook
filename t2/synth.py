ba669475161a2d50a7a1ee58ec36aecd79b3b3f3
davk789 <david.joseph.kendall@gmail.com>
2011-08-20 13:33:26 -0700

    more t2 audio refinements

--- t2/synth.py
+++ t2/synth.py
@@ -6,13 +6,24 @@ Run the sequencer from python here.
 """
 import time
 import random
+import threading
 from scosc import controller
 
 s = controller.Controller(("127.0.0.1", 57110))
 
-lo = 40
-hi = 1000
-beat = 2.0
+# all scales will be expressed as intervals
+root = 55.0
+scales = {'bohlen-pierce': (1.0/1.0,25.0/21.0,9.0/7.0,7.0/5.0,5.0/3.0,9.0/5.0,
+                            15.0/7.0,7.0/3.0,25.0/9.0,3.0/1.0),
+          'pythagorean':   (1.0/1.0, 9.0/8.0, 81.0/64.0,4.0/3.0,3.0/2.0,
+                            27.0/16.0,243.0/128.0, 2.0/1.0),
+          'partch':        (1.0/1.0,12.0/11.0,11.0/10.0,10.0/9.0,9.0/8.0,
+                            8.0/7.0,7.0/6.0,6.0/5.0,11.0/9.0,5.0/4.0,
+                            14.0/11.0,9.0/7.0,4.0/3.0,11.0/8.0,7.0/5.0,
+                            10.0/7.0,16.0/11.0,3.0/2.0,14.0/9.0,11.0/7.0,
+                            8.0/5.0,18.0/11.0,5.0/3.0,12.0/7.0,7.0/4.0,
+                            16.0/9.0,9.0/5.0,20.0/11.0,11.0/6.0,2.0/1)
+          }
 defs = ["ts_sin_touch",
         "ts_swoop",
         "ts_hash",
@@ -24,26 +35,58 @@ defs = ["ts_sin_touch",
         "ts_zwoop",
         "ts_zwerp",
         "ts_bausch",
+        "ts_bissosch",
+        "ts_wassd",
+        "ts_snxsd",
+        "ts_snoossd",
         ]
+
+def make_scale(notes):
+    ret = []
+    for note in notes:
+        if random.randrange(2):
+            ret.append(note)
+    
+    if not notes[-1] == ret[-1]:
+        ret.append(notes[-1])
+    return ret
+                
+
+def random_note(scale, octave=None):
+    note = random.choice(scale)
+    if not octave: octave = random.randrange(4)
+    omul = scale[-1] ** octave
+    return note * root * omul
+
+def refresh_scale(notes):
+    while True:
+        doloop.scale = make_scale(notes)
+        time.sleep(random.randrange(0, 16, 4))
 
 def doloop():
     while True:
-        for i in range(int(random.random() * 125)):
-            s.sendBundle(pow(random.random() * beat, 2),
+        print doloop.scale
+        for i in range(400):
+            beat = random.random() * random.choice([0.1, 0.2, 0.2, 0.4])
+            s.sendBundle(random.random() * beat,
                          [['s_new', random.choice(defs), -1, 0, 1,
-                           'freq',  random.uniform(lo, hi),
-                           'freq2', random.uniform(lo, hi),
+                           'freq',  random_note(doloop.scale),
+                           'freq2', random_note(doloop.scale),
                            'pan',   random.uniform(-1.0, 1.0),
                            'att',   random.uniform(0.0025, 0.04),
                            'rel',   random.uniform(0.1, 0.4),
-                           'lmod',  random.uniform(0.001, 20.0),
+                           'lmod',  random.uniform(0.001, 25.0),
                            'lfreq', random.uniform(1.0, 4.0),
                            'lag',   0.2,
-                           'lev',   random.uniform(0.01, 0.15),
+                           'lev',   random.uniform(0.01, 0.1),
                            'rez',   random.uniform(0.2, 0.8)
                            ]]),
-        time.sleep(random.random() * beat)
+        time.sleep(beat)
+doloop.scale = make_scale(scales["partch"])
+
 
 if __name__ == "__main__":
+    refresh = threading.Thread(target=refresh_scale, args=[scales["partch"]])
+    refresh.start()
     doloop()
 
