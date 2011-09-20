2f38197614c8b08c245e552f9a4f9cda322484fa
davk789 <david.joseph.kendall@gmail.com>
2011-09-20 13:20:17 -0700

    tuned the scale generation algo, enclosed the synth in a class

--- t2/synth.py
+++ t2/synth.py
@@ -20,7 +20,6 @@ from scosc import controller, tools, patterns
 threads = []
 s = [controller.Controller(("192.168.2.5", 57110)),
      controller.Controller(("192.168.2.5", 57210))]
-run_flag = Value('I', 1)
 # all scales will be expressed as intervals
 root = 55.0
 
@@ -65,7 +64,111 @@ synthdefs = ["ts_sin_touch",
              "ts_cubyyy",
              ]
 
-def make_scale(ratio, min=0.1, oct=2.0):
+class GranuSynth(Process):
+    """GranuSynth is a Process instance -- a granular synth loop with all 
+    necessary data encapsulated in the instance."""
+    def __init__(self, notes, face, defs):
+        Process.__init__(self)
+        self.notes = notes
+        self.data = face
+        self.defs = defs
+        self.can_run = Value('I', 1)
+        self.scale = self.make_fscale()
+        self.fseq = patterns.Pseq(self.scale)
+        # seems somehow un-pythonic to have such specialized (i.e. non-
+        # parametric) data initialized here like this
+        self.valcounter = 0 # used only by self.next_value()
+        self.unitval = 1.0 # used only by self.get_unit()
+
+    def run(self):
+        self.graingen()
+    
+    def stop(self):
+        self.can_run.value = 0
+        self.join()
+
+    def graingen(self):
+        delay = 1.0 # compensate for network latency
+        while self.can_run.value:
+            server = get_server() # cycle through available servers
+            basetime = time.time()
+            #for i in range(100):
+            beat = random.random() * random.choice([0.5, 0.25, 0.125])
+            for i in range(50):
+                #beat = random.random() * random.choice([0.1, 0.2, 0.2, 0.4])
+                
+                #beat = random.random() * random.choice([1.5, 1.25, 1.125])
+                # the quantization factor should be calculated from a face
+                quant = random.choice([4,8,16,8,8,2,32])
+                rbeat = random.randrange(quant) * (1.0/float(quant))# 0-1 value, quantized
+                server.sendBundleAbs(rbeat * beat + basetime + delay,
+                             [['s_new', random.choice(self.defs), -1, 0, 1,
+                               'freq',  self.ratio_to_freq(self.fseq.next()),
+                               'freq2', self.ratio_to_freq(self.fseq.next()),
+                               'pan',   self.next_value(-1.0, 1.0),
+                               'att',   random.uniform(0.0025, 0.01),
+                               'rel',   self.next_value(0.04, 0.4),
+                               'lmod',  self.next_value(0.001, 25.0),
+                               'lfreq', self.next_value(1.0, 4.0), # do not change range
+                               'lag',   0.2,
+                               'lev',   random.triangular(0.01, 0.1, 0.015),
+                               'rez',   self.next_value(0.2, 0.8)
+                               ]])
+            time.sleep(beat)
+            
+    def next_value(self, low, high):
+        if self.valcounter >= len(self.data):
+            self.valcounter = 0
+    
+        range = high - low
+        val = random.choice(self.data[self.valcounter]) * range + low
+        
+        self.valcounter += 1
+        return val
+
+    def make_fscale(self):
+        "make a scale with note choice based on the image data."
+        coef = 1.0 / self.get_unit(self.data)
+        seq = []
+        for point in self.data:
+            loc = [v * coef for v in point] 
+            index = loc[0] + (loc[1] * (coef+1))
+            seq.append(index)
+        result = [self.notes[int(i)%len(self.notes)] for i in seq]
+        return result
+
+    def ratio_to_freq(self, note):
+        "get frequency in a random octave for a specified ratio"
+        #octave = random.randrange(3) # 3 for tritave, 4 for octave scales
+        osize = self.notes[-1]
+        octave = random.randrange(4) # 3 for tritave, 4 for octave scales
+        omul = osize ** octave         # ******** octave size must match
+        return root * note * omul
+
+    def get_unit(self, data, max=1.0):
+        """get unit value in quantized multidimensional array. Assumes numbers or 
+        list/tuple only. Instead of making the facial analysis module deliver 
+        "grain" parameter directly, deduce the value algorithmically."""
+        self.unitval = max
+        for item in data:
+            if isinstance(item, (list, tuple)):
+                self.get_unit(item, self.unitval)
+            elif item < max and item > 0.0:
+                self.unitval = item
+        return self.unitval
+
+
+class Sampler(object):
+    """
+    Manage buffers and synth for a sampler voice to artificially thicken the sound.
+    """
+    def __init__(self):
+        pass
+    
+
+# *** GLOBAL UTILITY FUNCTIONS ***
+
+def make_scale(ratio, size=7, oct=2.0):
     """procedurally generate a just scale by stacking a single interval until
     it is no longer possible to add an interval larger than the specified 
     minimum."""
@@ -79,101 +182,14 @@ def make_scale(ratio, min=0.1, oct=2.0):
     # with other stacked ratios will exhibit the same behavior. So, this 
     # complicates writing melodies by indexing directly in to the scale list, 
     # which does not relate to my purpose.
-    while True:
+    for i in range(size):
         scale.append(note)
         note *= ratio
         if note > oct:
             note /= oct
-        if get_smallest_interval(scale) < min:
-            break
     scale.append(oct)
     scale.sort()
     return scale
-
-def get_smallest_interval(list):
-    "return the smallest nonzero interval between reference val and list"
-    min = max(list)
-    # calling sort here and in make_scale() is inefficient, but shouldn't be too
-    # much of a performance hit
-    slist = sorted(list)
-    for i in range(len(list)):
-        diff = slist[i] - slist[i-1]
-        if min > diff > 0.0:
-            min = diff
-    return min
-
-def make_rscale(notes):
-    "Make a scale by applying 50% probability to each note in a scale."
-    ret = []
-    for note in notes:
-        if random.randrange(2):
-            ret.append(note)
-    
-    if not notes[-1] == ret[-1]:
-        ret.append(notes[-1])
-    return ret
-
-def make_fscale(notes, fdata):
-    "make a scale with note choice based on the image data."
-    coef = 1.0 / get_unit(fdata)
-    seq = []
-    for point in fdata:
-        loc = [v * coef for v in point] 
-        index = loc[0] + (loc[1] * (coef+1))
-        seq.append(index)
-    result = [notes[int(i)%len(notes)] for i in seq]
-    return result
-
-def get_unit(data, max=1.0):
-    """get unit value in quantized multidimensional array. Assumes numbers or 
-    list/tuple only. Instead of making the facial analysis module deliver 
-    "grain" parameter directly, deduce the value algorithmically."""
-    get_unit.value = max
-    for item in data:
-        if isinstance(item, (list, tuple)):
-            get_unit(item, get_unit.value)
-        elif item < max and item > 0.0:
-            get_unit.value = item
-    return get_unit.value
-
-def ratio_to_freq(note, osize=2.0):
-    "get frequency in a random octave for a specified ratio"
-    #octave = random.randrange(3) # 3 for tritave, 4 for octave scales
-    octave = random.randrange(4) # 3 for tritave, 4 for octave scales
-    omul = osize ** octave         # ******** octave size must match
-    return root * note * omul
-
-def graingen(notes, face, defs, can_run):
-    delay = 1.0# compensate for network latency
-    scale = make_fscale(notes, face)
-    print scale
-    fseq = patterns.Pseq(scale)
-    while can_run.value:
-        server = get_server() # cyclt through available servers
-        basetime = time.time()
-        #for i in range(100):
-        beat = random.random() * random.choice([0.5, 0.25, 0.125])
-        for i in range(50):
-            #beat = random.random() * random.choice([0.1, 0.2, 0.2, 0.4])
-            
-            #beat = random.random() * random.choice([1.5, 1.25, 1.125])
-            # the quantization factor should be calculated from a face
-            quant = random.choice([4,8,16,8,8,2,32])
-            rbeat = random.randrange(quant) * (1.0/float(quant))# 0-1 value, quantized
-            server.sendBundleAbs(rbeat * beat + basetime + delay,
-                         [['s_new', random.choice(defs), -1, 0, 1,
-                           'freq',  ratio_to_freq(fseq.next(), notes[-1]),
-                           'freq2', ratio_to_freq(fseq.next(), notes[-1]),
-                           'pan',   next_value(face, -1.0, 1.0),
-                           'att',   random.uniform(0.0025, 0.01),
-                           'rel',   next_value(face, 0.04, 0.4),
-                           'lmod',  next_value(face, 0.001, 25.0),
-                           'lfreq', next_value(face, 1.0, 4.0), # do not change range
-                           'lag',   0.2,
-                           'lev',   random.triangular(0.01, 0.1, 0.015),
-                           'rez',   next_value(face, 0.2, 0.8)
-                           ]])
-        time.sleep(beat)
 
 def get_server():
     server = s[get_server.index]
@@ -181,19 +197,8 @@ def get_server():
     return server
 get_server.index = 0
 
-def next_value(data, low, high):
-    if next_value.count >= len(data):
-        next_value.count = 0
-
-    range = high - low
-    val = random.choice(data[next_value.count]) * range + low
-    
-    next_value.count += 1
-    return val
-next_value.count = 0
-
-def doloop(scale, face, defs, can_run):
-    threads.append(Process(target=graingen, args=[scale, face, defs, can_run]))
+def doloop(scale, face, defs):
+    threads.append(GranuSynth(scale, face, defs))
     threads[-1].start()
 
 def randsplit(data, sections=2):
@@ -204,6 +209,9 @@ def randsplit(data, sections=2):
     for item in data:
         ret[random.randrange(sections)].append(item)
     return ret
+
+# *** OSC LISTENER FUNCTIONS ***
+# ***   NOT USED RIGHT NOW   ***
         
 def start_listener():
     "This is how to receive data packets from a network."
@@ -229,40 +237,55 @@ def osc_value(key, data):
     "assuming a list is a set of osc message pairs, get value for tag"
     return data[data.index(key)+1]
 
+# *** INTERFACE COMMANDS ***
+
 def stop():
     "Stops all running synth loops."
-    global threads
-    run_flag.value = 0
+    global threads # check if I can get rid of this now
     for thread in threads:
-        thread.join()
-    threads = []
+        thread.stop()
     
 def run(faces):
     "start or stop a synth loop"
     numvoices = len(faces)
-    ratio = float(random.randrange(3, 13))
-    scale = make_scale(ratio / (ratio - 1), 0.1)
-    print scale
+    # only the first three will ever get selected
+    rchoices = [3.0/2.0, 5.0/4.0, 8.0/7.0, 9.0/8.0, 11.0/10.0, 13.0/12.0]
+    # doesn't check for bounding errors. watch for a bug here.
+    ratio = rchoices[len(faces)-1]
     if numvoices > 0:
-        run_flag.value = 1
         defs = randsplit(synthdefs, numvoices)
         for i in range(min(numvoices, 7)): # limit the number of synth voices to 6
+            scale = make_scale(ratio, len(faces[i]))
             # MAIN SYNTH LOOP
-            doloop(scale, faces[i], defs[i], run_flag)
+            doloop(scale, faces[i], defs[i])
             # **** ***** ****
     else:
         stop()
 
+# *** TESTING ***
+
 def test(numvoices=1, quant=4):
     data = []
     for i in range(numvoices):
-        face = [(random.randrange(0, quant) / float(quant-1),
-                 random.randrange(0, quant) / float(quant-1))
-                 for i in range(random.randrange(3, 15))]
+        face = random_face(quant)
         data.append(face)
     run(data)
+
+def test_scale(numvoices):
+    for i in range(numvoices):
+        face = random_face()
+        notes = make_scale(3.0/2.0, len(face))
+        print GranuSynth.make_fscale(notes, face)
+
+def random_face(quant=4):
+    return [(random.randrange(0, quant) / float(quant-1),
+            random.randrange(0, quant) / float(quant-1))
+            for i in range(random.randrange(3, 15))]
+
+# *** MAIN LOOP ***
 
 if __name__ == "__main__":
     #start_listener()
     test(2)
+    #test_scale(2)
 
